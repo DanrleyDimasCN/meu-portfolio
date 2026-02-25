4611fbfb7cb6173cf247bc5d6e87118cebe1eeab
Danrley <dd@hack.net.br>
2026-02-25 17:07:11 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "Meu Portfólio | Desenvolvedor de Software",
     description: "Um mostruário dos meus projetos e experiência em desenvolvimento de software.",
+    icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' },
+    ],
+  },
+  manifest: '/site.webmanifest', // O arquivo que veio no ZIP também vai aqui
 };
 
 export default function RootLayout({
